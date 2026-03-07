42a638f310eaa5ce58a187cd8982184ed0d28cb2
Shivam Rastogi <rastogi.shivam18@hotmail.com>
2026-03-07 19:38:59 +0530

    Implementation for insertion sort

---
+++ Sorting/InsertionSort.py
@@ -0,0 +1,15 @@
+class Solution:
+    def asc_sort(self, series):
+        length = len(series)
+        print(series)
+        for i in range(1, length):
+            key = series[i]
+            j = i-1
+            while j >= 0 and series[j] > key:
+                series[j+1] = series[j]
+                j = j-1 
+                print(series)
+            series[j+1] = key 
+            print(series)
+sol = Solution()
+sol.asc_sort([3, 5, 4, 6, 8, 9, 7, 10, 1])
